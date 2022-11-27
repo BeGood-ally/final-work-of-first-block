16ca48bb2cc583b330b2186694f7f1ada865916f
User <organik07@mail.ru>
2022-11-27 22:02:43 +0300

    добавлена функция создающая массив с выражениями длинной менее или равным трем символам

--- Program.cs
+++ Program.cs
@@ -10,3 +10,16 @@ int threeOrLess(string [] arr){
     }
     return count;
 }
+
+//Функция, создающая массив с выражениями длинной менее или равным 3 символам
+string [] arrOfThreeAndLess(int number, string [] beginArr){
+    string [] finalArr = new string[number];
+    int j = 0;
+    for (int i = 0; i < beginArr.Length; i++){
+        if(beginArr[i].Length < 4){
+            finalArr[j] = beginArr[i];
+            j++;
+        }
+    }
+    return finalArr;
+}
