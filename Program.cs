e11cb1ee96f0f543913da59ffbd9b3b4c2c18dc4
User <organik07@mail.ru>
2022-11-27 21:59:23 +0300

    добавлена функция выполняющая счет количества выражений с длинной не более 3 символов

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string [] arr1 = {"low", "thing", "-9", ":-)", "i", "Dave"};
+
+//Функция считающая количество ячеек в будущем массиве
+int threeOrLess(string [] arr){
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++){
+        if(arr[i].Length < 4){
+            count++;
+        }
+    }
+    return count;
+}
